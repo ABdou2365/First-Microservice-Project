d431b3c493254e25b8cee2b46e372b173128b108
ABDELLAH MOUTAWAKKIL <abdo.moutawakkil2002@gmail.com>
2025-06-11 02:07:47 +0100

    Functionality: Service got their configuration from the Config Service

--- customer-service/src/main/java/com/abdellah/customerservice/config/ConfigTestRestController.java
+++ customer-service/src/main/java/com/abdellah/customerservice/config/ConfigTestRestController.java
@@ -1,14 +1,15 @@
 package com.abdellah.customerservice.config;
 
 import com.abdellah.customerservice.entities.CustomerConfigProperties;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RefreshScope
 public class ConfigTestRestController {
 
     @Value("${global.params.p1}")
